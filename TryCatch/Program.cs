1e9dd12454e1a3d4a3550eb567343b8ab99952b9
k.ziarkowski <k.ziarkowski@PK3-16>
2019-09-17 17:43:28 +0200

    Add project files.

---
+++ TryCatch/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var name = ConfigurationManager.AppSettings["name"];
+            Console.WriteLine(name);
+
+            try
+            {
+                int a, b;
+                a = 2;
+                b = 0;
+                // int x = a / b;
+               
+                switch (name)
+                {
+                    case Names.roman:
+                        break;
+                    case Names.amma:
+                        break;
+                    case Names.adam:
+                        Console.WriteLine(name);
+                        break;
+                    default:
+                        break;
+                }
+
+                Test test = new Test();
+                test.x = 0; // 
+                Test.Name = "Roman";
+
+
+                throw new Exception("test error ");
+            }
+            catch (DivideByZeroException dbzero)
+            {
+                Console.WriteLine(dbzero.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            finally
+            {
+                Console.WriteLine("Koniec programu");
+            }    
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TryCatch/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+   public class Test
+    {
+        public int x;
+        public static string Name;
+    }
+    public enum Names 
+    {
+        roman,
+        amma = 2,
+        adam,
+    }
+}
